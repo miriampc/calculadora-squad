789c195cd1ba12212398691940b32c6031bbb5b7
miriampc <miiriampc@gmail.com>
2017-03-01 23:38:28 -0500

    implementando estructura HTML y enlazando a js

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+function operaciones(){
+  var num1=document.getElementById('num1').value;
+  var num2=document.getElementById('num2').value;
+  var resultado=document.getElementById('resultado');
+  var opcion=document.getElementById('select').value;
+  var sino=document.getElementById('sino').value;
+
+  
+}
+document.getElementById('form').onclick=function(e){
+  e.preventDefault();
+}
